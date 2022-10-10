347c019c23a4444b6039ce0a6313a0a9e5c0a313
Leon Fattakhov <leon.fattakhov@cockroachlabs.com>
2022-10-10 16:05:18 -0400

    metrics: expose pebble fsync latency as prometheus metric
    Leverage the metric callback exposed in pebble to update the store’s
    histogram. However since the callbacks must be setup in `pebble.Open`
    but the metric to update is part of the `kvserver/metric.go`. We store
    the callbacks on the `pebble.onMetricEvent pebble.MetricEventListener`
    and craft a struct that wraps the callbacks with a `nil` check to
    ensure that they have been set inside of `kvserver/store.go` store
    start method:`Store.Start()`. This allows for deferring the setting of
    the callback handlers until we have access to the metrics to update.
    
    Diagram of the above description:
    
    ```
         type Pebble struct {
           onMetricEvent struct {
      +--->  SomeCallback func(duration time.Duration) <--------------+
      |    }                                                          |
      |  }                                                            |
      |                                                               |  Checks
      |  func NewPebble(...) {                                        |
      |    ...                                                        |
      |    cfg.Opts.MetricEventListener = pebble.MetricEventListener{ |
      |      SomeCallback: func(duration time.Duration) {             |
    S |    	   if p.onMetricEvent.SomeCallback != nil { +-------------^
    e |    	     p.onMetricEvent.SomeCallback(duration) +-------------v
    t |    	   }                                                      |
    s |    	 },                                                       |
      |    }                                                          |
      |                                                               |
      |    db, err := pebble.Open(cfg.StorageConfig.Dir, cfg.Opts)    |
      |    ...                                                        |  Calls
      |  }                                                            |
      |                                                               |
      |  func (s *Store) Start(...) {                                 |
      |    ...                                                        |
      ^--- s.engine.RegisterMetricEventListener(                      |
             pebble.MetricEventListener{                              |
               SomeCallback: func(duration time.Duration) {           |
                 s.metrics.FsyncLatency.RecordValue(duration)<--------v
               }
             })
           ...
         }
    ```
    
    Release note: None

--- pkg/kv/kvserver/metrics.go
+++ pkg/kv/kvserver/metrics.go
@@ -1669,6 +1669,13 @@ Note that the measurement does not include the duration for replicating the eval
 		Measurement: "Flush Utilization",
 		Unit:        metric.Unit_PERCENT,
 	}
+
+	metaPebbleFsyncLatency = metric.Metadata{
+		Name:        "pebble.fsync.latency",
+		Help:        "The pebble write ahead log writer fsync latency",
+		Measurement: "Fsync Latency",
+		Unit:        metric.Unit_NANOSECONDS,
+	}
 )
 
 // StoreMetrics is the set of metrics for a given store.
@@ -1965,6 +1972,7 @@ type StoreMetrics struct {
 	ReplicaWriteBatchEvaluationLatency *metric.Histogram
 
 	FlushUtilization *metric.GaugeFloat64
+	FsyncLatency     *metric.Histogram
 }
 
 type tenantMetricsRef struct {
@@ -2503,6 +2511,7 @@ func newStoreMetrics(histogramWindow time.Duration) *StoreMetrics {
 			metaReplicaWriteBatchEvaluationLatency, histogramWindow, metric.IOLatencyBuckets,
 		),
 		FlushUtilization: metric.NewGaugeFloat64(metaPebbleFlushUtilization),
+		FsyncLatency:     metric.NewHistogram(metaPebbleFsyncLatency, histogramWindow, metric.IOLatencyBuckets),
 	}
 
 	{
